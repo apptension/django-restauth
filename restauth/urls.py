5ad7541d645add26c77eef7e48c98e9a768f50b2
Aleksander Ambrozkiewicz <aambrozkiewicz@apptension.com>
2018-03-30 13:49:15 +0200

    Append slashes

--- restauth/urls.py
+++ restauth/urls.py
@@ -7,17 +7,17 @@ from rest_framework_jwt.views import obtain_jwt_token
 from . import views
 
 user_patterns = [
-    path('signup', views.SignUpView.as_view(), name='signup'),
-    path('token', obtain_jwt_token, name='jwt_token'),
-    path('me', views.UserProfileView.as_view(), name='profile'),
-    path('confirm', views.UserAccountConfirmationView.as_view(), name='confirmation'),
-    path('change-password', views.UserAccountChangePasswordView.as_view(), name='change_password')
+    path('signup/', views.SignUpView.as_view(), name='signup'),
+    path('token/', obtain_jwt_token, name='jwt_token'),
+    path('me/', views.UserProfileView.as_view(), name='profile'),
+    path('confirm/', views.UserAccountConfirmationView.as_view(), name='confirmation'),
+    path('change-password/', views.UserAccountChangePasswordView.as_view(), name='change_password')
 ]
 
 password_reset_patterns = [
     path('', views.PasswordResetView.as_view(), name='password_reset'),
-    path(r'confirm', views.PasswordResetConfirmationView.as_view(), name='password_reset_confirmation'),
-    path(r'confirm/<user_pk>/<token>', views.PasswordResetConfirmationView.as_view(),
+    path(r'confirm/', views.PasswordResetConfirmationView.as_view(), name='password_reset_confirmation'),
+    path(r'confirm/<user_pk>/<token>/', views.PasswordResetConfirmationView.as_view(),
          name='password_reset_confirmation_url')
 ]
 
