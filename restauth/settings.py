e03907d2296f0368672e49fb80f56ee2a9c2d105
Aleksander Ambrozkiewicz <aambrozkiewicz@apptension.com>
2018-03-30 14:09:42 +0200

    Change auth backends

--- restauth/settings.py
+++ restauth/settings.py
@@ -122,8 +122,8 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.BasicAuthentication',
     ),
     'DEFAULT_THROTTLE_RATES': {
         'anon': '100/day'
