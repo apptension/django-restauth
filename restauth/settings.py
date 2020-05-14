859bbd494d0eda7912a6dea58eafb50ffce1c2fb
Aleksander Ambrozkiewicz <aambrozkiewicz@apptension.com>
2020-05-14 13:10:21 +0200

    Allowed hosts env

--- restauth/settings.py
+++ restauth/settings.py
@@ -19,7 +19,7 @@ environ.Env.read_env()
 SECRET_KEY = env("SECRET_KEY")
 
 DEBUG = env("DEBUG")
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
 
 
 # Application definition
